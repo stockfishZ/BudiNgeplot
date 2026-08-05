3cef780cede2883e5531ed07e7b687e69b65d9b0
Rafa Satria Pratama <rafasatria2512@gmail.com>
2026-08-05 22:58:13 +0700

    refactor: rename app.html to index.html and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,21 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath } from 'node:url';
-import { renameSync } from 'node:fs';
 import react from '@vitejs/plugin-react';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
   plugins: [
     react(),
-    viteSingleFile(),
-    {
-      name: 'output-as-index-html',
-      closeBundle() {
-        renameSync('dist/app.html', 'dist/index.html');
-      }
-    }
+    viteSingleFile()
   ],
   base: './',
   build: {
     rollupOptions: {
-      input: fileURLToPath(new URL('./app.html', import.meta.url))
+      input: fileURLToPath(new URL('./index.html', import.meta.url))
     }
   },
   server: {
     port: 3000,
-    open: '/app.html'
+    open: '/index.html'
   }
 });
